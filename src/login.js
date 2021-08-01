85c49be7f5f8876c5e7309962e9e1eb260acf673
adaglory <a.ogbonna@ymail.com>
2021-08-01 08:38:07 +0000

    login states
    states have been added to the login page

--- src/login.js
+++ src/login.js
@@ -1,7 +1,16 @@
 import { Button, Container, Form, Input } from "reactstrap";
 import "./css/login.css";
+import { useState } from "react";
 import xtraclass from "./images/xtra-class.png";
 const Login = () => {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const validateForm = () => {
+    return user.length > 0 && password.length > 0;
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className='login'>
       <Container>
@@ -9,12 +18,12 @@ const Login = () => {
         <div className='login__form'>
           <h3>Login</h3>
           <hr />
-          <Form className='login__inputs'>
-            <Input type='email' name='email' id='exampleEmail' placeholder='User' />
+          <Form className='login__inputs' onSubmit={handleSubmit}>
+            <Input type='email' name={user} id='user' placeholder='User' onChange={(e) => setUser(e.target.value)} />
 
-            <Input type='password' name='password' id='examplePassword' placeholder='Password' />
+            <Input type='password' value={password} id='password' placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
 
-            <Button color='primary' size='lg' block>
+            <Button color='primary' size='lg' disabled={!validateForm()}>
               Login
             </Button>
           </Form>
